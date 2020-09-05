4331d0672375b5de19b92d3abe3d1b414c5f2a30
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-09-05 20:53:43 +0545

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path=require('path')
+module.exports={
+    mode:"production",
+    entry:'./src/App.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:"output.js"
+    },
+    devServer:{
+        contentBase:path.join(__)
+
+    }
+
+}
